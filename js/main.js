1d757325bc9dae56f1ac18fcabcd8083944ea188
LeandervanAarde <200211@virtualwindow.co.za>
2022-01-25 22:05:38 +0200

    Login and signup for lg devices done
    The lg devices log in screen has been made, the styling is complete @EpicBlue1 you can edit any of this at any point :) more refining to come

--- js/main.js
+++ js/main.js
@@ -4,12 +4,28 @@ $(function(){
     setTimeout(() => {
         // prelaoder to fade out after the delay
         $('.preloader-container').delay(650).fadeOut(3000);
-        // $('.login-container').fadeIn(4000)
+        $('.login-container').fadeIn(4000)
     }, 7000);
     // Sign up form function 
 
     $('#sign-up').on('click', () =>{
-        console.log("sign up form clicked")
+        console.log("sign up form clicked");
+        $('.login-container').slideUp(800);
+        setTimeout(() => {
+           
+            $('.reg-container').slideDown(800)
+        }, 850);
+
+    });
+
+    $('#login-btn-2').on('click', () =>{
+        console.log("login form clicked");
+        $('.reg-container ').slideUp(800);
+        setTimeout(() => {
+           
+        $('.login-container').slideUp(800);
+            $('.login-container ').slideDown(800)
+        }, 850);
 
     });
   
